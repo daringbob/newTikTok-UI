c01a97d5bdba427a7231ae731c7124e31711e683
Thanh <quocthanh056@gmail.com>
2023-08-25 18:03:31 +0700

    Cài fontawsome,normalize thêm css cơ bản.

--- src/Components/Layout/DefaultLayout/Sidebar/index.js
+++ src/Components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import classNames from "classnames/bind";
+import React from "react";
+
+import styles from "./Sidebar.module.scss";
+
+const cx = classNames.bind(styles);
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+  return <div className={cx("wrapper")}>Sidebar</div>;
 }
 
-export default Sidebar
+export default Sidebar;
